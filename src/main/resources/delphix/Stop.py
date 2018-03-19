0a31600dc06f4ee43764057d75fd69891a3b2dd5
smouhssine <saidi.mouhssine@gmail.com>
2018-03-19 08:48:18 -0400

    modified DelphixClient.py

---
+++ src/main/resources/delphix/Stop.py
@@ -0,0 +1,26 @@
+#
+# Copyright 2018 XEBIALABS + MSA (Mouhssine SAIDI COE DELPHIX)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+
+if __name__ == '__main__':
+    import sys
+
+#    sys.path.append('/Users/bmoussaud/Workspace/xebialabs-community/xlr-delphix-plugin/src/main/resources')
+    sys.path.append('/u02/app/xebialabs/xlr-delphix-plugin/src/main/resources')
+#    server = {'url': 'http://ba5b5824.ngrok.io', 'username': 'delphix_admin', 'password': 'landshark'}
+    server = {'url': 'http://192.168.247.132', 'username': 'delphix_admin', 'password': 'landshark'}
+    vdb = 'XEBIA'
+
+from delphix.DelphixClient import DelphixClient
+
+
+client = DelphixClient(server)
+output = client.stop(vdb)
+job = output['job']
+action = output['action']
