c26552568ecb06f2cfe3b7b897aadf9f3ba2e79b
Joris De Winne <joris.dewinne@gmail.com>
2018-10-19 16:21:26 -0700

    Adding an automated test for list bookmarks

--- src/main/resources/delphix/DelphixClient.py
+++ src/main/resources/delphix/DelphixClient.py
@@ -7,205 +7,50 @@
 #
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
-
-import requests
 import json
 
+from delphixpy.web.selfservice import bookmark
+from delphixpy.web.selfservice import branch
 
-class DelphixClient:
+from delphix.lib.DelphixSession import DelphixSession
+from delphix.lib.GetReferences import find_obj_name
+import delphix.lib.Util as Util
+
+
+class DelphixClient(object):
 
     def __init__(self, server):
-        self.server = server
-        self.cookies = self.get_session()
+        self.engine = DelphixSession.create(server).server_session
 
-    def get_session(self):
-        print('- open session on {0}'.format(self.server['url']))
-        headers = {'Content-type': 'application/json'}
-        data = {
-            "type": "APISession",
-            "version": {
-                "type": "APIVersion",
-                "major": 1,
-                "minor": 8,
-                "micro": 2
-            }
-        }
 
-        r = requests.post('{0}/resources/json/delphix/session'.format(self.server['url']),
-                          headers=headers,
-                          data=json.dumps(data))
-        return r.cookies
+    @staticmethod
+    def create_client(http_connection):
+        return DelphixClient(http_connection)
 
-    def _login(self):
-        print('- login')
-        headers = {'Content-type': 'application/json'}
-        data = {
-            "type": "LoginRequest",
-            "username": self.server['username'],
-            "password": self.server['password'],
-        }
-        r = requests.post('{0}/resources/json/delphix/login'.format(self.server['url']),
-                          headers=headers,
-                          cookies=self.cookies,
-                          data=json.dumps(data))
+    def delphix_listbookmarks(self, variables):
+        js_bookmarks = bookmark.get_all(self.engine)
 
-        if self._response_ok(r):
-            print("- Authenticated ! ")
-        else:
-            raise self._response_error(r)
-        return r.text
+        path = Util.split_path(variables['template_path'])
 
-    def _logout(self):
-        print('- logout')
-        headers = {'Content-type': 'application/json'}
-        requests.post('{0}/resources/json/delphix/logout'.format(self.server['url']),
-                      headers=headers,
-                      cookies=self.cookies)
+        result = []
+        for js_bookmark in js_bookmarks:
+            if js_bookmark.template_name != path['template_name']:
+                continue
 
-    def _get_params(self, vdb):
-        result = {'ref': 'NOT_FOUND', 'cont': 'NOT_FOUND', 'vsrc': 'NOT_FOUND'}
-        print('- get params')
-        headers = {'Content-type': 'application/json'}
+            add_it = False
+            if variables['tag_filter'] is None:
+                tag = js_bookmark.tags if js_bookmark.tags else None
+                add_it = True
+            else:
+                for tag in variables['tag_filter']:
+                    if tag in js_bookmark.tags:
+                        add_it = True
+                        break
 
-        r = requests.get('{0}/resources/json/delphix/database'.format(self.server['url']),
-                         headers=headers,
-                         cookies=self.cookies)
+            if add_it:
+                branch_name = find_obj_name(self.engine, branch, js_bookmark.branch)
+                key = Util.form_bookmark_path(js_bookmark.template_name, js_bookmark.container_name, branch_name, js_bookmark.name)
+                result.append(key)
+                print "%s\n" % key
 
-        for db in r.json()['result']:
-            if db['name'] == vdb:
-                result['ref'] = db['reference']
-                result['cont'] = db['provisionContainer']
-
-        r = requests.get('{0}/resources/json/delphix/source'.format(self.server['url']),
-                         headers=headers,
-                         cookies=self.cookies)
-
-        for db in r.json()['result']:
-            if db['name'] == vdb:
-                result['vsrc'] = db['reference']
-
-        print('- database reference {ref}'.format(**result))
-        print('- database provisionContainer {cont}'.format(**result))
-        print('- source reference {vsrc}'.format(**result))
-        return result
-
-    def _rewind(self, vdb):
-        parameters = self._get_params(vdb=vdb)
-        print("- rewind ")
-        headers = {'Content-type': 'application/json'}
-        data = {
-            "type": "OracleRollbackParameters",
-            "timeflowPointParameters": {
-                "type": "TimeflowPointSemantic",
-                "container": parameters['ref'],
-                "location": "LATEST_SNAPSHOT"
-            }
-        }
-
-        r = requests.post(
-            '{0}/resources/json/delphix/database/{1}/rollback'.format(self.server['url'], parameters['ref']),
-            headers=headers,
-            cookies=self.cookies,
-            data=json.dumps(data))
-        if self._response_ok(r):
-            print("- Job    {0}".format(r.json()['job']))
-            print("- Action {0}".format(r.json()['action']))
-            return {'job': r.json()['job'], 'action': r.json()['action']}
-        else:
-            raise self._response_error(r)
-
-    def _snapshot(self, vdb):
-        parameters = self._get_params(vdb=vdb)
-        print("- snapshot ")
-        headers = {'Content-type': 'application/json'}
-        data = {
-            "type": "OracleSyncParameters"
-        }
-
-        r = requests.post(
-            '{0}/resources/json/delphix/database/{1}/sync'.format(self.server['url'], parameters['ref']),
-            headers=headers,
-            cookies=self.cookies,
-            data=json.dumps(data))
-
-        if self._response_ok(r):
-            print("- Job    {0}".format(r.json()['job']))
-            print("- Action {0}".format(r.json()['action']))
-            return {'job': r.json()['job'], 'action': r.json()['action']}
-        else:
-            raise self._response_error(r)
-
-    def _stop(self, vdb):
-        parameters = self._get_params(vdb=vdb)
-        print("- stop ")
-        headers = {'Content-type': 'application/json'}
- #       data = {
- #           "type": "OracleSyncParameters"
- #       }
-
-        r = requests.post(
-            '{0}/resources/json/delphix/database/{1}/stop'.format(self.server['url'], parameters['ref']),
-            headers=headers,
-            cookies=self.cookies,
- #           data=json.dumps(data))
-
-        if self._response_ok(r):
-            print("- Job    {0}".format(r.json()['job']))
-            print("- Action {0}".format(r.json()['action']))
-            return {'job': r.json()['job'], 'action': r.json()['action']}
-        else:
-            raise self._response_error(r)
-
-    def _start(self, vdb):
-        parameters = self._get_params(vdb=vdb)
-        print("- snapshot ")
-        headers = {'Content-type': 'application/json'}
- #       data = {
- #           "type": "OracleSyncParameters"
- #       }
-
-        r = requests.post(
-            '{0}/resources/json/delphix/database/{1}/start'.format(self.server['url'], parameters['ref']),
-            headers=headers,
-            cookies=self.cookies)
-#            data=json.dumps(data))
-
-        if self._response_ok(r):
-            print("- Job    {0}".format(r.json()['job']))
-            print("- Action {0}".format(r.json()['action']))
-            return {'job': r.json()['job'], 'action': r.json()['action']}
-        else:
-            raise self._response_error(r)
-    def _response_ok(self, r):
-        return r.status_code == 200 and r.json()['type'] == 'OKResult'
-
-    def _response_error(self, r):
-        return Exception(r.text)
-
-    def snapshot(self, vdb):
-        self._login()
-        print("- Snapshot {0} with Delphix".format(vdb))
-        result = self._snapshot(vdb)
-        self._logout()
-        return result
-
-    def rewind(self, vdb):
-        self._login()
-        print("- Rewind {0} with Delphix".format(vdb))
-        result = self._rewind(vdb)
-        self._logout()
-        return result
-		
-    def stop(self, vdb):
-        self._login()
-        print("- Stop {0} with Delphix".format(vdb))
-        result = self._stop(vdb)
-        self._logout()
-        return result
-
-    def start(self, vdb):
-        self._login()
-        print("- start {0} with Delphix".format(vdb))
-        result = self._start(vdb)
-        self._logout()
-        return result	
+        variables['bookmarks'] = result
